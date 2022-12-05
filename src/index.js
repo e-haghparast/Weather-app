1b43a87d6dcb516e25d808f9af9ed0a16ce27317
e-haghparast <73380762+e-haghparast@users.noreply.github.com>
2022-12-05 23:12:16 +0330

    updated time

--- src/index.js
+++ src/index.js
@@ -1,47 +1,41 @@
 let apikey = "bfd67b65e01f8c3751ffb4a48f09d863";
-
-let time = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-
-let day = days[time.getDay()];
-let currentDay = document.querySelector("#current-day");
-currentDay.innerHTML = day;
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", showCity);
-function updateTime() {
-  let hour = time.getHours();
-  if (hour < 10) {
-    hour = `0${hour}`;
+function FormatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
-  let minute = time.getMinutes();
-  if (minute < 10) {
-    minute = `0${minute}`;
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
-  let clock = document.querySelector("#clock");
-  clock.innerHTML = `${hour}:${minute}`;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}${hours}:${minutes}`;
 }
 function showCity(event) {
-  event?.preventDefault();
-
+  event.preventDefault();
   // currentCity.innerHTML = searchInput.value;
-
   let currentCity = document.querySelector("#current-city");
   let searchInput = document.querySelector("#search-input");
-  updateTime();
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apikey}&units=metric`;
   axios.get(url).then(updateData);
 }
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", showCity);
 
 function updateData(response) {
+  let currentCity = document.querySelector("#current-city");
+
+  console.log(response);
   let cityName = response.data.name;
   let humidity = response.data.main.humidity;
   let wind = Math.round(response.data.wind.speed);
@@ -50,9 +44,12 @@ function updateData(response) {
   let temperatureMax = Math.round(response.data.main.temp_max);
   let temperatureMin = Math.round(response.data.main.temp_min);
   let humidityStatus = document.querySelector("#humidity-status");
+  currentCity.innerHTML = cityName;
   humidityStatus.innerHTML = `Humidity:${humidity}%`;
   let windStatus = document.querySelector("#wind-status");
   windStatus.innerHTML = `Wind:${wind}mph`;
+  let date = document.querySelector("#date");
+  date.innerHTML = FormatDate(response.data.dt * 1000);
   let condition = document.querySelector("#condition");
   condition.innerHTML = `${description}`;
   let limitationFirst = document.querySelector("#limitation-h");
@@ -61,14 +58,11 @@ function updateData(response) {
   limitationSecond.innerHTML = `L:${temperatureMin}°`;
   let tempNumber = document.querySelector("#temp-number");
   tempNumber.innerHTML = `${temperature}°c`;
-  let currentCity = document.querySelector("#current-city");
-  currentCity.innerHTML = cityName;
 }
 
 function retrieveLocation(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-  updateTime();
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`;
   axios.get(url).then(updateData);
 }
