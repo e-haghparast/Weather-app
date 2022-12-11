c66fdf90fdb36e4eb9a8eeb1d92dc3dd42a37556
e-haghparast <73380762+e-haghparast@users.noreply.github.com>
2022-12-11 11:53:04 +0330

    removed fahrenheit unit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-"use strict";
 function FormatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -41,11 +40,11 @@ function displayForecast(response) {
 
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div>`;
-  forecast.forEach(function (forecastDay, index ) {
-if (index < 6){
-  forecastHTML =
-    forecastHTML +
-    `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="forecast-wrapper">
       <div class="forecast">
         <span class="forecast-day">${formatDay(forecastDay.dt)}</span><br/>
@@ -61,9 +60,8 @@ if (index < 6){
       </div>
     </div>
   `;
-}
+    }
   });
-
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
@@ -116,30 +114,7 @@ function showCity(event) {
   search(searchInput.value);
 }
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", showCity);
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 search("Tehran");
